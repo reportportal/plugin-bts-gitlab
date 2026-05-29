98ed79acd79aeca81e47d5fd754461f9d85c0fa3
Evelina Sarkisian <59954853+Evelina02@users.noreply.github.com>
2026-05-29 12:37:24 +0300

    Release 5.13.2 (#50)

--- src/main/java/com/epam/reportportal/extension/gitlab/utils/GitlabObjectMapperProvider.java
+++ src/main/java/com/epam/reportportal/extension/gitlab/utils/GitlabObjectMapperProvider.java
@@ -3,28 +3,28 @@ package com.epam.reportportal.extension.gitlab.utils;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.Getter;
 
 /**
  * @author Zsolt Nagyaghy
  */
+@Getter
 public class GitlabObjectMapperProvider {
-    private final ObjectMapper objectMapper;
 
-    public GitlabObjectMapperProvider() {
-        this.objectMapper = new ObjectMapper();
+  private final ObjectMapper objectMapper;
 
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+  public GitlabObjectMapperProvider() {
+    this.objectMapper = new ObjectMapper();
 
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        objectMapper.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true);
-    }
+    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
 
-    public ObjectMapper getObjectMapper() {
-        return objectMapper;
-    }
+    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    objectMapper.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true);
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    objectMapper.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true);
+  }
+
 }
